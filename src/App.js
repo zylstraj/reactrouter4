6c2ce668f87d57ba9dff058df4bc80e5753bad2f
zylstraj <colbertlove12@gmail.com>
2017-03-30 14:33:51 -0700

    Playing Around with React-Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const App = () => <h1>Hello World</h1>
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
+const Away = () => <h1>Not here</h1>
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{pathname: "/about"}}>About</Link>
+    <Link to="/Away">Away</Link>
+    </nav>
+)
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/away" component={Away} />
+    </div>
+  </Router>
+
+)
 
 export default App
